4e93634c7441154b761160834ac70f0397c6aa74
PeggyHung <98597379+PeggyHung@users.noreply.github.com>
2022-05-31 00:30:18 +0800

    change dummyUser to currentUser in vuex

--- src/components/RestaurantComments.vue
+++ src/components/RestaurantComments.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <h2 class="my-4">
-      所有評論：
-    </h2>
+    <h2 class="my-4">所有評論：</h2>
 
     <div v-for="comment in restaurantComments" :key="comment.id">
       <blockquote class="blockquote mb-0">
@@ -15,62 +13,54 @@
           Delete
         </button>
         <h3>
-          <router-link :to="{name: 'user', params: { id: comment.User.id }}">
-            {{comment.User.name}}
+          <router-link :to="{ name: 'user', params: { id: comment.User.id } }">
+            {{ comment.User.name }}
           </router-link>
         </h3>
-        <p>{{comment.text}}</p>
+        <p>{{ comment.text }}</p>
         <footer class="blockquote-footer">
-          {{comment.createdAt | fromNow}}
+          {{ comment.createdAt | fromNow }}
         </footer>
       </blockquote>
-      <hr>
+      <hr />
     </div>
   </div>
 </template>
 
 <script>
-import {fromNowFilter} from './../utils/mixins'
-import restaurantsAPI from './../apis/restaurants'
-import { Toast } from './../utils/helpers'
-
-const dummyUser = {
-  currentUser: {
-    id: 1,
-    name: '管理者',
-    email: 'root@example.com',
-    image: 'https://i.pravatar.cc/300',
-    isAdmin: true
-  },
-  isAuthenticated: true
-}
+import { fromNowFilter } from "./../utils/mixins";
+import restaurantsAPI from "./../apis/restaurants";
+import { Toast } from "./../utils/helpers";
+import { mapState } from "vuex";
 
 export default {
   mixins: [fromNowFilter],
   props: {
     restaurantComments: {
       type: Array,
-      required: true
-    }
+      required: true,
+    },
   },
-  data() {
-    return {
-      currentUser: dummyUser.currentUser
-    }
+  computed: {
+    ...mapState(["currentUser"])
   },
   methods: {
     async handleDeleteButtonClick(commentId) {
       try {
-        const response = await restaurantsAPI.deleteComment({ commentId })
-        console.log('response', response)
-        this.$emit('after-delete-comment', commentId)
+        const {data, statusText} = await restaurantsAPI.deleteComment({ commentId });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText)
+        }
+
+        this.$emit("after-delete-comment", commentId);
       } catch (error) {
-         Toast.fire({
-          icon: 'warning',
-          title: '無法刪除餐廳評論，請稍後再試'
-        })
+        Toast.fire({
+          icon: "warning",
+          title: "無法刪除餐廳評論，請稍後再試",
+        });
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
