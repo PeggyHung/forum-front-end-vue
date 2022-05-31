e0f5536f4f2b46c07c3e15c8568e5c7e8ec3d93b
PeggyHung <98597379+PeggyHung@users.noreply.github.com>
2022-05-31 15:57:55 +0800

    Optimize verification of user authorization

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,17 @@ import NotFound from '../views/NotFound.vue'
 import Restaurants from '../views/Restaurants.vue'
 import store from './../store'
 
+Vue.use(VueRouter)
+
+const authorizeIsAdmin = (to, from, next) => {
+  const currentUser = store.state.currentUser
+  if (currentUser && !currentUser.isAdmin) {
+    next('/404')
+    return
+  }
+  next()
+}
+
 const routes = [
   {
     path: '/',
@@ -69,32 +80,38 @@ const routes = [
   {
     path: '/admin/restaurants',
     name: 'admin-restaurants',
-    component: () => import('../views/AdminRestaurants.vue')
+    component: () => import('../views/AdminRestaurants.vue'),
+    beforeEnter: authorizeIsAdmin 
   },
   {
     path: '/admin/restaurants/new',
     name: 'admin-restaurants-new',
-    component: () => import('../views/AdminRestaurantNew.vue')
+    component: () => import('../views/AdminRestaurantNew.vue'),
+    beforeEnter: authorizeIsAdmin
   },
   {
     path: '/admin/restaurants/:id/edit',
     name: 'admin-restaurant-edit',
-    component: () => import('../views/AdminRestaurantEdit.vue')
+    component: () => import('../views/AdminRestaurantEdit.vue'),
+    beforeEnter: authorizeIsAdmin
   },
   {
     path: '/admin/restaurants/:id',
     name: 'admin-restaurant',
-    component: () => import('../views/AdminRestaurant.vue')
+    component: () => import('../views/AdminRestaurant.vue'),
+    beforeEnter: authorizeIsAdmin
   },
   {
     path: '/admin/categories',
     name: 'admin-categories',
-    component: () => import('../views/AdminCategories.vue')
+    component: () => import('../views/AdminCategories.vue'),
+    beforeEnter: authorizeIsAdmin
   },
   {
     path: '/admin/users',
     name: 'admin-users',
-    component: () => import('../views/AdminUsers.vue')
+    component: () => import('../views/AdminUsers.vue'),
+    beforeEnter: authorizeIsAdmin
   },
   {
     path: '*',
@@ -103,18 +120,35 @@ const routes = [
   }
 ]
 
-Vue.use(VueRouter)
-
 const router = new VueRouter({
   linkExactActiveClass: 'active',
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  // console.log('to', to)
-  // console.log('from', from)
-  // 使用 dispatch 呼叫 Vuex 內的 actions
-  store.dispatch('fetchCurrentUser')
+router.beforeEach( async (to, from, next) => {
+  const tokenInLocalStorage = localStorage.getItem('token')
+  const tokenInStore = store.state.token
+  let isAuthenticated = store.state.isAuthenticated
+ 
+  // 比較 localStorage 和 store 中的 token 是否一樣
+  if (tokenInLocalStorage && tokenInLocalStorage !== tokenInStore) {
+    isAuthenticated = await store.dispatch('fetchCurrentUser')
+  }
+  
+  // 對於不需要驗證 token 的頁面
+  const pathWithoutAuthentication = ['sign-up', 'sign-in']
+
+  // 如果 token 無效則轉址到登入頁
+  if (!isAuthenticated && !pathWithoutAuthentication.includes(to.name)) {
+    next('/signin')
+    return
+  }
+  // 如果 token 有效則轉址到餐廳首頁
+  if (isAuthenticated && pathWithoutAuthentication.includes(to.name)) {
+    next('/restaurants')
+    return
+  }
+
   next()
 })
 
--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,9 @@ export default new Vuex.Store({
       image: '',
       isAdmin: false
     },
-    isAuthenticated: false
+    isAuthenticated: false,
+    // 新增 token 屬性
+    token: ''
   },
   getters: {
   },
@@ -23,23 +25,34 @@ export default new Vuex.Store({
         ...state.currentUser,
         ...currentUser
       }
+      // 將使用者驗證用的 token 儲存在 state 中
+      state.token = localStorage.getItem('token')
       state.isAuthenticated = true
+    },
+    revokeAuthentication(state) {
+      state.currentUser = {}
+      state.isAuthenticated = false
+      // 登出時一併將 state 內的 token 移除
+      state.token = ''
+      localStorage.removeItem('token')
     }
   },
   actions: {
     // 在 actions 中可以透過參數的方式取得 commit 的方法
     async fetchCurrentUser({ commit }) {
       try {
-        // 呼叫 usersAPI.getCurrentUser() 方法，並將 response 顯示出來 (確認data中有登入使用者的資料)
-        // const response = await usersAPI.getCurrentUser()
-        // console.log('response', response)
         const {data} = await usersAPI.getCurrentUser()
 
         const {id, name, email, image, isAdmin} = data
 
         commit('setCurrentUser', { id, name, email, image,  isAdmin})
+
+        return true
       } catch (error) {
-        console.error(error.message)
+        console.error('can not fetch user information')
+        // 驗證失敗的話一併觸發登出的行為，以清除 state 中的 token
+        commit('revokeAuthentication')
+        return false
       }
     }
   },
